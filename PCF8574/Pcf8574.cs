a640f50a96bbe51c9b6cd1ea203ac18b4eebbeba
wastegate711 <evgeni.shvec@gmail.com>
2022-12-25 22:03:00 +0300

    Дописал функционал и немного рефакторил.

--- PCF8574/Pcf8574.cs
+++ PCF8574/Pcf8574.cs
@@ -1,19 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Windows.Devices.Enumeration;
 using Windows.Devices.Gpio;
 using Windows.Devices.I2c;
 
-namespace PCF8574
+namespace PortExpanders.PCF8574
 {
     public class Pcf8574 : IPcf8574, IDisposable
     {
         private I2cDevice _pcf8574;
-        GpioPin _interrupt;
+        private GpioPin _interrupt;
 
+        /// <inheritdoc />
+        public event Action<byte> PinChanged;
+
+
+        /// <inheritdoc />
         public void Dispose()
         {
             _pcf8574.Dispose();
@@ -21,7 +24,7 @@ namespace PCF8574
         }
 
         /// <inheritdoc />
-        public async void Initialization(int address, int pinNumber = int.MaxValue, I2cBusSpeed busSpeed = I2cBusSpeed.StandardMode)
+        public async Task<bool> Initialization(int address, I2cBusSpeed busSpeed = I2cBusSpeed.StandardMode)
         {
             var connectSetting = new I2cConnectionSettings(address);
             connectSetting.BusSpeed = busSpeed;
@@ -29,48 +32,105 @@ namespace PCF8574
             _pcf8574 = controller.GetDevice(connectSetting);
 
             if (_pcf8574 == null)
-                throw new NullReferenceException("_pcf8574");
+                return false;
 
-            if (pinNumber != int.MaxValue)
-            {
-                var gpioController = await GpioController.GetDefaultAsync();
-                _interrupt = gpioController.OpenPin(pinNumber);
-                _interrupt.Write(GpioPinValue.High);
-                _interrupt.SetDriveMode(GpioPinDriveMode.OutputOpenDrainPullUp);
-                _interrupt.ValueChanged += Interrupt_ValueChanged;
-            }
+            return true;
         }
 
         /// <inheritdoc />
-        public async void Initialization(string interfaceName,
+        public async Task<bool> Initialization(
+            string interfaceName,
             int address,
-            int pinNumber = int.MaxValue,
             I2cBusSpeed busSpeed = I2cBusSpeed.StandardMode)
         {
-            var list = I2cDevice.GetDeviceSelector(interfaceName);
+            var aqs = I2cDevice.GetDeviceSelector(interfaceName);
+            var info = await DeviceInformation.FindAllAsync(aqs);
             var connectSetting = new I2cConnectionSettings(address);
             connectSetting.BusSpeed = busSpeed;
-            var devInfo = await DeviceInformation.FindAllAsync(list);
-            _pcf8574 = await I2cDevice.FromIdAsync(devInfo.First().Id, connectSetting);
+            _pcf8574 = await I2cDevice.FromIdAsync(info.First().Id, connectSetting);
 
             if (_pcf8574 == null)
-                throw new NullReferenceException("_pcf8574");
+                return false;
 
-            if (pinNumber != int.MaxValue)
-            {
-                var gpioController = await GpioController.GetDefaultAsync();
-                _interrupt = gpioController.OpenPin(pinNumber);
-                _interrupt.Write(GpioPinValue.High);
-                _interrupt.SetDriveMode(GpioPinDriveMode.OutputOpenDrainPullUp);
-                _interrupt.ValueChanged += Interrupt_ValueChanged;
-            }
+            return true;
+        }
+
+        /// <inheritdoc />
+        public async Task<bool> Initialization(int address, int pinInterrupt, I2cBusSpeed busSpeed = I2cBusSpeed.StandardMode)
+        {
+            var connectSetting = new I2cConnectionSettings(address);
+            connectSetting.BusSpeed = busSpeed;
+            var controller = await I2cController.GetDefaultAsync();
+            _pcf8574 = controller.GetDevice(connectSetting);
+
+            var gpioController = await GpioController.GetDefaultAsync();
+            _interrupt = gpioController.OpenPin(pinInterrupt);
+
+            if (_pcf8574 == null | _interrupt == null)
+                return false;
+
+            _interrupt.SetDriveMode(GpioPinDriveMode.InputPullUp);
+            _interrupt.DebounceTimeout = TimeSpan.FromMilliseconds(1);
+            _interrupt.ValueChanged += Interrupt_ValueChanged;
+
+            return true;
+        }
+
+        /// <inheritdoc />
+        public async Task<bool> Initialization(
+            string interfaceName,
+            int address,
+            int pinInterrupt,
+            I2cBusSpeed busSpeed = I2cBusSpeed.StandardMode)
+        {
+            var aqs = I2cDevice.GetDeviceSelector(interfaceName);
+            var info = await DeviceInformation.FindAllAsync(aqs);
+            var connectSetting = new I2cConnectionSettings(address);
+            connectSetting.BusSpeed = busSpeed;
+            _pcf8574 = await I2cDevice.FromIdAsync(info.First().Id, connectSetting);
+
+            var gpioController = await GpioController.GetDefaultAsync();
+            _interrupt = gpioController.OpenPin(pinInterrupt);
+
+            if (_pcf8574 == null)
+                return false;
+
+            _interrupt.SetDriveMode(GpioPinDriveMode.InputPullUp);
+            _interrupt.DebounceTimeout = TimeSpan.FromMilliseconds(1);
+            _interrupt.ValueChanged += Interrupt_ValueChanged;
+
+            return true;
+        }
+
+        /// <inheritdoc />
+        public void SetPinState(byte pin)
+        {
+            byte[] rxData = new byte[1];
+            byte[]txData= new byte[1];
+
+            _pcf8574.Read(rxData);
+            txData[0] = (byte)(rxData[0] ^ pin);
+            _pcf8574.Write(txData);
+        }
+
+        /// <inheritdoc />
+        public byte ReadPinState()
+        {
+            byte[] rxData = new byte[1];
+
+            _pcf8574.Read(rxData);
+
+            return rxData[0];
         }
 
         private void Interrupt_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
         {
+            byte[] rxData = new byte[1];
+
             if (args.Edge == GpioPinEdge.FallingEdge)
             {
-
+                _pcf8574.Read(rxData);
+                PinChanged?.Invoke(rxData[0]);
             }
         }
     }
