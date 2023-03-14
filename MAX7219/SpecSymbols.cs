c01594333791c03a13b04359b4560d46378921ad
wastegate711 <evgeni.shvec@gmail.com>
2023-03-14 15:31:44 +0300

    #12  Добавил перечисление с кодами символов.

---
+++ MAX7219/SpecSymbols.cs
@@ -0,0 +1,34 @@
+﻿namespace MAX7219
+{
+    public enum SpecSymbols : byte
+    {
+        /// <summary>
+        /// Символ "-"
+        /// </summary>
+        Minus = 0x0A,
+        /// <summary>
+        /// Символ "Е"
+        /// </summary>
+        E = 0x0B,
+        /// <summary>
+        /// Символ "Н"
+        /// </summary>
+        H = 0x0C,
+        /// <summary>
+        /// Символ "L"
+        /// </summary>
+        L = 0x0D,
+        /// <summary>
+        /// Символ "Р"
+        /// </summary>
+        P = 0x0E,
+        /// <summary>
+        /// Пустой символ(ничего не светится)
+        /// </summary>
+        Empty = 0x0F,
+        /// <summary>
+        /// Зажигает точку в указанном сегменте.
+        /// </summary>
+        Dot = 0x80
+    }
+}
